2e68ee37f07a2700b22f51f15b5bb0a85b82492b
RouSage <therousage@gmail.com>
2018-05-11 15:36:55 +0300

    Application DbContext added. Db file created. Connection string added

---
+++ BlogApp.Repo/EFBlogAppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BlogApp.Data;
+using System.Data.Entity;
+
+namespace BlogApp.Repo
+{
+    public class EFBlogAppDbContext : DbContext
+    {
+        public EFBlogAppDbContext() : base("EFBlogAppDbContext")
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>()
+                .HasMany<Tag>(p => p.Tags)
+                .WithMany(t => t.Posts)
+                .Map(tp =>
+                {
+                    tp.MapLeftKey("PostRefID");
+                    tp.MapRightKey("TagRefID");
+                    tp.ToTable("PostTag");
+                });
+        }
+    }
+}

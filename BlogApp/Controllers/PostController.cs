324d21bf3eece910d2dd461d7d52e4ec982dbedd
RouSage <therousage@gmail.com>
2018-05-11 20:37:30 +0300

    Classes for pagination added

---
+++ BlogApp/Controllers/PostController.cs
@@ -0,0 +1,27 @@
+﻿using BlogApp.Models;
+using BlogApp.Service;
+using System.Web.Mvc;
+using BlogApp.Data;
+using System.Collections.Generic;
+
+namespace BlogApp.Controllers
+{
+    [RequireHttps]
+    public class PostController : Controller
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        // GET: Post
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Latest Posts";
+
+            return View();
+        }
+    }
+}

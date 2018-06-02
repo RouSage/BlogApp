d8a59b9a69ea8ee5327dd11e5283e0406e41bb9d
RouSage <therousage@gmail.com>
2018-06-02 16:28:23 +0300

    AdminController

---
+++ BlogApp/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogApp.Controllers
+{
+    [RequireHttps]
+    [Authorize]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Admin/Manage")]
+        public ActionResult Manage()
+        {
+            return View();
+        }
+    }
+}
